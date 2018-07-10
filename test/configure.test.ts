fa029607c46a3640ffdde4b949a41ca9cbbd2a0d
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-07-10 16:34:58 -0700

    work

--- test/configure.test.ts
+++ test/configure.test.ts
@@ -23,53 +23,45 @@ import { globAsync } from '../helpers/async';
 suite("configure (Add Docker files to Workspace)", function (this: ITestCallbackContext) {
     this.timeout(60 * 1000);
     let rootFolder = vscode.workspace.workspaceFolders[0].uri.fsPath;
-    const testFolderPath: string = path.join(rootFolder, `configureTests${randomness.getRandomHexString()}`);
+    // asdf const testFolderPath: string = path.join(rootFolder, `configureTests${randomness.getRandomHexString()}`);
     const outputChannel: vscode.OutputChannel = vscode.window.createOutputChannel('Docker extension tests');
     ext.outputChannel = outputChannel;
 
-    suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
-        console.log(`Test folder: ${testFolderPath}`);
-        await fse.ensureDir(testFolderPath);
-    });
-
-    suiteTeardown(async () => {
-        await fse.remove(testFolderPath);
-    });
-
-    let projectFolderPath: string;
-    async function testInFolder(folderName: string, func: () => Promise<void>) {
-        test(folderName, async () => {
-            projectFolderPath = path.join(testFolderPath, folderName);
-            await fse.ensureDir(projectFolderPath);
-            await func();
-        });
-    }
-
-    async function testConfigureDocker(folderPath: string, platform: Platform, ...inputs: (string | undefined)[]): Promise<void> {
+    async function testConfigureDocker(platform: Platform, ...inputs: (string | undefined)[]): Promise<void> {
         // Set up simulated user input
         inputs.unshift(platform);
         const ui: TestUserInput = new TestUserInput(inputs);
         ext.ui = ui;
 
-        await fse.ensureDir(folderPath);
-        await configure(folderPath);
+        await configure(rootFolder);
         assert.equal(inputs.length, 0, 'Not all inputs were used.');
     }
 
     async function writeFile(fileName: string, text: string): Promise<void> {
-        await fse.writeFile(path.join(projectFolderPath, 'package.json'), text);
+        await fse.writeFile(path.join(rootFolder, 'package.json'), text);
     }
 
     function fileContains(fileName: string, text: string): boolean {
-        let contents = fse.readFileSync(path.join(projectFolderPath, fileName)).toString();
+        let contents = fse.readFileSync(path.join(rootFolder, fileName)).toString();
         return contents.search(text) >= 0;
     }
 
-    testInFolder("Node.js no package.json", async () => {
-        await testConfigureDocker(projectFolderPath, 'Node.js', '1234');
-        let files = await globAsync('**/*', { cwd: projectFolderPath });
+    async function getProjectFiles(): Promise<string[]> {
+        return await globAsync('**/*', { cwd: rootFolder });
+    }
 
-        assertEx.unorderedArraysEqual(files, ['Dockerfile', 'docker-compose.debug.yml', 'docker-compose.yml'], "The set of files in the project folder after configure was run is not correct.");
+    function testInFolder(name: string, func: () => Promise<void>): void {
+        test(name, async () => {
+            await fse.emptyDir(rootFolder);
+            await func();
+        });
+    }
+
+    testInFolder("Node.js no package.json", async () => {
+        await testConfigureDocker('Node.js', '1234');
+        let projectFiles = await getProjectFiles();
+
+        assertEx.unorderedArraysEqual(projectFiles, ['Dockerfile', 'docker-compose.debug.yml', 'docker-compose.yml'], "The set of files in the project folder after configure was run is not correct.");
 
         assert(fileContains('Dockerfile', 'EXPOSE 1234'));
         assert(fileContains('Dockerfile', 'CMD npm start'));
@@ -107,8 +99,8 @@ suite("configure (Add Docker files to Workspace)", function (this: ITestCallback
             }
             `);
 
-        await testConfigureDocker(projectFolderPath, 'Node.js', '4321');
-        let files = await globAsync('**/*', { cwd: projectFolderPath });
+        await testConfigureDocker('Node.js', '4321');
+        let files = await getProjectFiles();
 
         assert(fileContains('Dockerfile', 'EXPOSE 4321'));
         assert(fileContains('Dockerfile', 'CMD npm start'));
@@ -145,10 +137,35 @@ suite("configure (Add Docker files to Workspace)", function (this: ITestCallback
             }
             `);
 
-        await testConfigureDocker(projectFolderPath, 'Node.js', '4321');
-        let files = await globAsync('**/*', { cwd: projectFolderPath });
+        await testConfigureDocker('Node.js', '4321');
+        let files = await getProjectFiles();
 
         assert(fileContains('Dockerfile', 'EXPOSE 4321'));
         assert(fileContains('Dockerfile', 'CMD node ./out/dockerExtension'));
     });
+
+    testInFolder("ASP.NET Core empty folder", async () => {
+        await writeFile('package.json',
+            `{
+                "name": "vscode-docker",
+                "version": "0.0.28",
+                "main": "./out/dockerExtension",
+                "author": "Azure",
+                "scripts": {
+                  "vscode:prepublish": "tsc -p ./",
+                  "test": "npm run build && node ./node_modules/vscode/bin/test"
+                },
+                "dependencies": {
+                  "azure-arm-containerregistry": "^1.0.0-preview"
+                }
+            }
+            `);
+
+        await testConfigureDocker('ASP.NET Core', '1234');
+        let files = await getProjectFiles();
+
+        assert(fileContains('Dockerfile', 'EXPOSE 4321'));
+        assert(fileContains('Dockerfile', 'CMD node ./out/dockerExtension'));
+    });
+
 });
--- test/testSetup.ts
+++ test/testSetup.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import * as assert from "assert";
+import * as path from "path";
 
 export function testSetup(): void {
     let workspaceFolders = vscode.workspace.workspaceFolders;
@@ -8,5 +8,9 @@ export function testSetup(): void {
     }
     if (workspaceFolders.length > 1) {
         throw new Error("There are unexpected multiple workspaces open");
+    }
+
+    if (path.dirname(workspaceFolders[0].uri.fsPath) !== '.testoutput') {
+        throw new Error("vscode is opened against the wrong folder for tests");
     }
 }
